dab437cd695f8c76ceb0d9afcb579fde7daf6ffa
johnpdowling <john.patrick.dowling@gmail.com>
2017-02-15 21:26:56 -0800

    first run of interaction lifecycle

--- fishbrains.ino
+++ fishbrains.ino
@@ -20,17 +20,35 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-//  digitalWrite(HEAD, digitalRead(IN_ONE)); 
-//  digitalWrite(MOUTH, digitalRead(IN_TWO)); 
-//  digitalWrite(TAIL, digitalRead(IN_TWO));
+  bool one = (digitalRead(IN_ONE) == HIGH);
+  bool two = (digitalRead(IN_TWO) == HIGH);
 
-  digitalWrite(HEAD, HIGH); 
-  digitalWrite(MOUTH, HIGH); 
-  digitalWrite(TAIL, HIGH); 
-  delay(2000);
-  digitalWrite(HEAD, LOW); 
-  digitalWrite(MOUTH, LOW); 
-  digitalWrite(TAIL, LOW);
-  delay(1000);
+  if(one && !two)
+  {
+    //recording mode. just keep head up
+    digitalWrite(HEAD, HIGH);
+    digitalWrite(MOUTH, LOW);
+    digitalWrite(TAIL, LOW);
+  }
+  else if(!one && two)
+  {
+    //playback mode. head up, tail down, mouth moving
+    digitalWrite(HEAD, HIGH);
+    digitalWrite(MOUTH, HIGH);
+    digitalWrite(TAIL, LOW);
+  }
+  else if(one && two)
+  {
+    //processing mode. head up, tail moving, mouth shut
+    digitalWrite(HEAD, HIGH);
+    digitalWrite(MOUTH, LOW);
+    digitalWrite(TAIL, HIGH);
+  }
+  else
+  {
+    //back to normal
+    digitalWrite(HEAD, LOW);
+    digitalWrite(MOUTH, LOW);
+    digitalWrite(TAIL, LOW);
+  }
 }
